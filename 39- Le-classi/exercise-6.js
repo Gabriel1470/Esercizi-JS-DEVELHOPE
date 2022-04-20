a5fd85972ad8258bed9810201a3104678023eacd
Gabriel Bogdan <gabriel1470bg@gmail.com>
2022-04-20 16:19:25 +0200

    Es:39

---
+++ 39- Le-classi/exercise-6.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(figura){
+    if(figura.side){
+      return (figura.side * figura.side);
+    }else if (figura.width,figura.height){
+return (figura.width * figura.height);
+    }else if (figura.radius){
+      return (figura.radius ** 2 * Math.PI)
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
